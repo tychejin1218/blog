9b1eabfb1215de4ee949eac349634dadb23d4aa7
A-Bong <tychejin@naver.com>
2022-07-08 13:54:00 +0900

    Feat: Stream max() 사용법

---
+++ stream/src/stream/Example06.java
@@ -0,0 +1,22 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ *
+ */
+public class Example06 {
+
+  public static void main(String[] args){
+
+    System.out.println("1.max() 사용법");
+    createStreamMax();
+  }
+
+  public static void createStreamMax(){
+    List<String> strings = Arrays.asList("Python", "Java", "PHP", "JavaScript", "Kotlin");
+
+
+  }
+}
