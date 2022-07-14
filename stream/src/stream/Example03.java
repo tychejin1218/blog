7c5313f54768b77c3cc02c04f39f457d3335e2dd
A-Bong <tychejin@naver.com>
2022-07-14 13:35:36 +0900

    Feat: Stream filter(), distinct() 사용법

--- stream/src/stream/Example03.java
+++ stream/src/stream/Example03.java
@@ -11,15 +11,27 @@ public class Example03 {
 
   public static void main(String[] args) {
 
-    System.out.println("1.distinct() 사용법");
+    System.out.println("1.filter() 사용법");
+    createStreamFilter();
+
+    System.out.println("2.distinct() 사용법");
     createStreamDistinct();
   }
 
   /*
-   * distinct()은 원본 스트림에서 중복을 제거하고 스트림을 생성하여 리턴
+   * filter()는 해당 스트림에서 주어진 조건(predicate)에 맞는 요소만으로 구성된 새로운 스트림을 반환
+   */
+  public static void createStreamFilter() {
+    Stream<String> stream1 = Stream.of("Python", "Java", "PHP", "JavaScript", "Kotlin");
+    Stream<String> stream2 = stream1.filter(s -> "Java".equals(s));
+    stream2.forEach(System.out::println);
+  }
+
+  /*
+   * distinct()은 해당 스트림에서 중복을 제거하고 새로운 스트림을 반환
    */
   public static void createStreamDistinct() {
-    List<String> strings = Arrays.asList("Python", "Java",  "C", "Python", "Java");
+    List<String> strings = Arrays.asList("Python", "Java", "C", "Python", "Java");
     Stream<String> stream1 = strings.stream();
     Stream<String> stream2 = stream1.distinct();
     stream2.forEach(System.out::println);
