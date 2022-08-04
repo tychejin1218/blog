96647d0e188099c4a8d21732feb4b113a33ac07f
A-Bong <tychejin@naver.com>
2022-08-04 16:56:48 +0900

    Feat: 백준 온라인 저지(Baekjoon Online Judge) 15649, 15650 문제 풀이

--- baekjoon/src/bruteforce/fastcampus/solution15650.java
+++ baekjoon/src/bruteforce/fastcampus/solution15650.java
@@ -1,14 +1,9 @@
-package bruteforce;
+package bruteforce.fastcampus;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class problem15649_1 {
+public class solution15650 {
 
   static StringBuilder sb = new StringBuilder();
 
@@ -21,20 +16,25 @@ public class problem15649_1 {
 
   static int N, M;
   static int[] selected;
+
   // Recurrence Function (재귀 함수)
   // 만약 M 개를 전부 고름   => 조건에 맞는 탐색을 한 가지 성공한 것!
   // 아직 M 개를 고르지 않음 => k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
   static void rec_func(int k) {
     if (k == M + 1) { // 1 ~ M 번째를 전부 다 골랐다!
       // selected[1...M] 배열이 새롭게 탐색된 결과
-      for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
+      for (int i = 1; i <= M; i++) {
+        sb.append(selected[i]).append(' ');
+      }
       sb.append('\n');
     } else {
       for (int cand = 1; cand <= N; cand++) {
         boolean isUsed = false;
-        for (int i=1;i<k;i++)
-          if (cand == selected[i])
+        for (int i = 1; i < k; i++) {
+          if (cand == selected[i]) {
             isUsed = true;
+          }
+        }
         // k 번째에 cand 가 올 수 있으면
         if (!isUsed) {
           selected[k] = cand;
@@ -56,6 +56,7 @@ public class problem15649_1 {
 
 
   static class FastReader {
+
     BufferedReader br;
     StringTokenizer st;
 
