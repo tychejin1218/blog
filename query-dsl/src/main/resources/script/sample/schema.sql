1212917644c0478fb5804998cf9939c0384a58f8
너나들이 <tychejin@naver.com>
2023-01-16 11:16:34 +0900

    Chore: Spring Data JPA + QueryDSL 설정

--- query-dsl/src/main/resources/script/sample/schema.sql
+++ query-dsl/src/main/resources/script/sample/schema.sql
@@ -1,4 +1,22 @@
-/*CREATE USER daekyo PASSWORD 'password1!' SUPERUSER CREATEDB;*/
+/*
+// MySQL 컨테이너 BASH 접속
+docker exec -it mysql /bin/bash
+
+// MySQL에 root 계정으로 접속
+mysql -u root -p
+
+// Database 생성
+CREATE DATABASE sample;
+
+// 계정 생성
+CREATE USER 'sample_user'@'%' IDENTIFIED BY 'password1!';
+
+// 권한 확인
+SHOW GRANTS FOR 'sample_user'@'%';
+
+// 권한 설정
+GRANT ALL PRIVILEGES ON sample.* TO 'sample_user'@'%';
+*/
 
 DROP TABLE IF EXISTS todo;
 
